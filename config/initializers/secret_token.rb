2ae6dc4bd162552e283933ebdddb7cdb8bcc6cb8
Nagy Bence <nagybence@tipogral.hu>
2012-02-09 09:44:37 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openmeetup::Application.config.secret_token = 'cc2b2fc4373cfeef9ebc8b5553821db376cc2eecaa18a786bd1e8bbd2e86ab76d31f58cebcaaaf1bab419df890db799958d8e51d4e36f542d99c21dd80eedc4d'
