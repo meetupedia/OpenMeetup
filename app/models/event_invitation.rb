34d0eb61267fff30c7cf3a4fdfd1d4a9917183e1
Nagy Bence <nagybence@tipogral.hu>
2012-04-04 10:35:53 +0200

    invitations emails field -> attribute

--- app/models/event_invitation.rb
+++ app/models/event_invitation.rb
@@ -1,12 +1,13 @@
 # -*- encoding : utf-8 -*-
 
 class EventInvitation < ActiveRecord::Base
-  key :emails, :as => :text
   key :message, :as => :text
   timestamps
 
   belongs_to :user
   belongs_to :event
+
+  attr_accessor :emails
 end
 
 EventInvitation.auto_upgrade!
--- app/models/group_invitation.rb
+++ app/models/group_invitation.rb
@@ -1,12 +1,14 @@
 # -*- encoding : utf-8 -*-
 
 class GroupInvitation < ActiveRecord::Base
-  key :emails, :as => :text
+#  key :emails, :as => :text
   key :message, :as => :text
   timestamps
 
   belongs_to :user
   belongs_to :group
+
+  attr_accessor :emails
 end
 
 GroupInvitation.auto_upgrade!
