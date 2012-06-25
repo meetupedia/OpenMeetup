a2d3242d453435f6a5614cd4dff6a42901461cdf
Nagy Bence <nagybence@tipogral.hu>
2012-06-25 11:05:28 +0200

    small fixes

--- app/controllers/discovery_controller.rb
+++ app/controllers/discovery_controller.rb
@@ -1,7 +1,7 @@
 class DiscoveryController < ApplicationController
 
   def index
-    @groups = Group.joins(:tags).where('groups.name LIKE ? OR tags.name LIKE ?', "%#{params[:q]}%", "#{params[:q]}%") if params[:q]
+    @groups = Group.joins(:tags).where('groups.name LIKE ? OR tags.name LIKE ?', "%#{params[:q]}%", "#{params[:q]}%").group('groups.id') if params[:q]
     Rails.logger.info @groups.inspect
   end
 
