c44ee008550bcec4abff3b89f4e3f0d6b6b21e7a
Nagy Bence <nagybence@tipogral.hu>
2012-08-24 09:09:45 +0200

    fixed abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,7 +11,7 @@ class Ability
       end
 
       can [:create, :update, :destroy], Event do |event|
-        event.group.admins.include?(current_user)
+        event.group.admins.include?(current_user) or current_user.is_admin?
       end
       can :invited, Event do |event|
         event.participants.include?(current_user)
@@ -21,7 +21,7 @@ class Ability
 
       can :create, Group
       can [:update, :destroy], Group do |group|
-        group.admins.include?(current_user)
+        group.admins.include?(current_user) or current_user.is_admin?
       end
       can [:invited, :waves], Group do |group|
         group.members.include?(current_user)
