5f07ca9f63a45366407ef4bb4d7f5e2e9a13992c
Nagy Bence <nagybence@tipogral.hu>
2012-09-04 10:39:09 +0200

    added MailUrlizer

--- app/models/common_mailer.rb
+++ app/models/common_mailer.rb
@@ -1,5 +1,28 @@
 # encoding: UTF-8
 
+module MailUrlizer
+
+  def self.included(base)
+    base.class_eval do
+      alias_method_chain :collect_responses_and_parts_order, :reformat
+    end
+  end
+
+  def collect_responses_and_parts_order_with_reformat(headers, &block)
+    responses, order = collect_responses_and_parts_order_without_reformat(headers, &block)
+    [responses.map { |response| reformat(response) }, order]
+  end
+
+  def reformat(response)
+    if response[:content_type] == 'text/html'
+      response.merge :body => response[:body].gsub('href="/', %{href="http://#{Settings.host}/})
+    else
+      response
+    end
+  end
+end
+
+
 class CommonMailer < ActionMailer::Base
   helper :common
   helper :application
