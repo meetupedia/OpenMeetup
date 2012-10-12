72f6b987213531cd6e0b04ad6676dfe1340aeb8b
Nagy Bence <nagybence@tipogral.hu>
2012-10-12 11:53:23 +0200

    mailer fix

--- app/controllers/event_invitations_controller.rb
+++ app/controllers/event_invitations_controller.rb
@@ -25,7 +25,7 @@ class EventInvitationsController < CommonController
         if event_invitation.new_record?
           event_invitation.message = @event_invitation.message
           if event_invitation.save
-            EventMailer.invitation(event_invitation).deliver
+            EventInvitationMailer.invitation(event_invitation).deliver
           end
         end
       end
