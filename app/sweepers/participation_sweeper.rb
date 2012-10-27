4d68d3f48e459b96c17a2ff383150c5e639be5ea
Nagy Bence <nagybence@tipogral.hu>
2012-10-27 12:26:23 +0200

    join fix

--- app/sweepers/participation_sweeper.rb
+++ app/sweepers/participation_sweeper.rb
@@ -4,7 +4,7 @@ class ParticipationSweeper < ActionController::Caching::Sweeper
   def after_create(participation)
     Activity.create_from participation, participation.user, participation.event.group, participation.event
     participation.event.absence_for(participation.user).andand.destroy
-    membership = participation.event.group.memberships.create :user => participation.user unless participation.event.group.membership_for(current_user)
+    membership = participation.event.group.memberships.create :user => participation.user unless participation.event.group.membership_for(participation.user)
     run_later do
       participation.event.group.admins.each do |user|
         EventMailer.participation(particiation, user).deliver if user.email
