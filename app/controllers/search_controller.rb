28942557ef644191edeb08cfd2f1e16e1b9d9250
frkandris <andris@szimpatikus.hu>
2012-12-14 15:39:51 +0100

    mistype

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,6 @@ class SearchController < CommonController
     @groups = Group.where('name LIKE ? OR description LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order('name ASC').limit(10)
     @events = Event.where('title LIKE ? OR description LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%").order('start_time DESC').limit(10)
     @interests = Interest.where('name LIKE ?', "%#{params[:q]}%").order('name ASC').limit(10)
-    @users = User.where('name LIKE ? OR nickname LIKE ? OR email LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%",).order('name ASC').limit(10)
+    @users = User.where('name LIKE ? OR nickname LIKE ? OR email LIKE ?', "%#{params[:q]}%", "%#{params[:q]}%", "%#{params[:q]}%").order('name ASC').limit(10)
   end
 end
