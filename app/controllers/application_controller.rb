417d8e1462be48283eb38a7625f4fbda3655f1d2
Nagy Bence <nagybence@tipogral.hu>
2013-01-09 08:28:26 +0100

    added mobile views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,6 +61,24 @@ private
     end
   end
 
+  def check_for_mobile
+    session[:mobile_override] = params[:mobile] if params[:mobile]
+    prepare_for_mobile if mobile_device?
+  end
+
+  def prepare_for_mobile
+    prepend_view_path Rails.root + 'app' + 'views_mobile'
+  end
+
+  def mobile_device?
+    if session[:mobile_override]
+      session[:mobile_override] == '1'
+    else
+      request.user_agent =~ /iPhone|iPod|Android|webOS|Mobile/ and request.user_agent !~ /iPad/
+    end
+  end
+  helper_method :mobile_device?
+
   def current_locale
     if params[:locale]
       session[:locale] = params[:locale]
