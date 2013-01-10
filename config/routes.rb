ba1784df1bc9fa96128841621ddbd6a0d83fa091
Nagy Bence <nagybence@tipogral.hu>
2013-01-10 12:08:06 +0100

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -160,7 +160,7 @@ Openmeetup::Application.routes.draw do
   match '/dashboard' => 'root#dashboard', :as => :dashboard
   match '/restricted_access' => 'root#restricted_access', :as => :restricted_access
 
-  match '/system' => 'system#index', :as => :system
+  match '/settings' => 'system#index', :as => :system
   match '/reload' => 'system#reload', :as => :reload
   match '/download_database' => 'system#download_database', :as => :download_database
 
