17ea038490e695fc85092c00f39aa245fde5f34a
frkandris <andris@szimpatikus.hu>
2013-02-20 09:52:45 +0100

    testing

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,27 +1,26 @@
 # load rails
 require File.expand_path(File.dirname(__FILE__) + "/environment") 
 
-# set path
-env :PATH, ENV['PATH']
-env :PATH, '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
-
 # sitemap refresh
 every 1.day, :at => '5:00 am' do
   rake "sitemap:refresh"
 end
 
 # git pull
-every '0,15,30,45 * * * *' do  
+# every '0,15,30,45 * * * *' do  
+every 5.minutes do
   command "cd #{path} && git pull https://github.com/meetupedia/OpenMeetup.git"
 end
 
 # bundle install
-every '5,20,35,50 * * * *' do
+# every '5,20,35,50 * * * *' do
+every 5.minutes do
   command "cd #{path} && bundle install"
 end
 
 # whenever update cron
-every '10,25,40,55 * * * *' do
+# every '10,25,40,55 * * * *' do
+every 5.minutes do
   command "cd #{path} && whenever --update-crontab"
 end
 
