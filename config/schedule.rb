0d90f58ff9267c4aaf1e77083fa04df8b7afe251
frkandris <andris@szimpatikus.hu>
2013-02-20 09:33:15 +0100

    whenever extend

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,39 +1,30 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
-
+# set path
 env :PATH, ENV['PATH']
 
+# load rails
+require File.expand_path(File.dirname(__FILE__) + "/environment") 
+
+# sitemap refresh
 every 1.day, :at => '5:00 am' do
   rake "sitemap:refresh"
 end
 
+# git pull
+every '0,15,30,45 * * * *' do  
+  command "cd #{path} && git pull https://github.com/meetupedia/OpenMeetup.git"
+end
+
+# bundle install
+every '5,20,35,50 * * * *' do
+  command "cd #{path} && bundle install"
+end
+
+# whenever update cron
+every '10,25,40,55 * * * *' do
+  command "cd #{path} && whenever --update-crontab"
+end
+
+# i am still alive?
 every 5.minutes do
   runner 'UserMailer.set_admin(User.find_by_email("andris@szimpatikus.hu")).deliver'
 end
-
-every '0,15,30,45 * * * *' do
-  command 'git pull https://github.com/meetupedia/OpenMeetup.git'
-end
-
-every '5,20,35,50 * * * *' do
-  command 'bundle install'
-end
-
