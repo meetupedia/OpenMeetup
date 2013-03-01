1fd8a9a3c39dfc89312454dce4870ee0c6575506
frkandris <andris@szimpatikus.hu>
2013-03-01 15:58:11 +0100

    schedule path fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,27 +8,22 @@ end
 # git pull
 # every '0,15,30,45 * * * *' do  
 every 5.minutes do
-  command "cd :path && git pull https://github.com/meetupedia/OpenMeetup.git master"
+  command "cd #{path} && git pull https://github.com/meetupedia/OpenMeetup.git master"
 end
 
 # bundle install
 # every '5,20,35,50 * * * *' do
 every 5.minutes do
-  command "source \"$HOME/.rvm/scripts/rvm\" && cd :path && bundle install"
+  command "source \"$HOME/.rvm/scripts/rvm\" && cd #{path} && bundle install"
 end
 
 # whenever update cron
 # every '10,25,40,55 * * * *' do
 every 5.minutes do
-  command "source \"$HOME/.rvm/scripts/rvm\" && cd :path && whenever --update-crontab"
+  command "source \"$HOME/.rvm/scripts/rvm\" && cd #{path} && whenever --update-crontab"
 end
 
 # i am still alive?
-# every 1.day, :at => '15:45' do
-#  runner 'User.set_admin(User.find_by_email("eva.turi@ericsson.com"))'
-# end
-
-# i am still alive?
-# every 5.minutes, :at => '15:45' do
+# every 10.minutes do
 #  runner 'UserMailer.set_admin(User.find_by_email("andris@szimpatikus.hu")).deliver'
 # end
