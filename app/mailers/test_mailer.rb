13b1ff39a4c17110c1d03db6f283685f0618181f
Nagy Bence <nagybence@tipogral.hu>
2013-03-04 15:31:57 +0100

    test_mailer improvements

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,8 +1,11 @@
 # encoding: UTF-8
 
-class TestMailer < CommonMailer
+class TestMailer < ActionMailer::Base
+  default_url_options[:host] = Settings.host
+  default :from => Settings.default_email
 
   def report(email)
+    attachment['production.log'] = File.read('/var/www/get2gather/log/production.log')
     mail :to => email, :subject => 'report from test_mailer'
   end
 end
