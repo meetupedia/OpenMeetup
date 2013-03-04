f5b052aad7c15b2acebe3d850670b9717f3a0ffc
Nagy Bence <nagybence@tipogral.hu>
2013-03-04 15:55:32 +0100

    fixed schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,33 +1,22 @@
+set :job_template, "bash -l -c 'source \"$HOME/.rvm/scripts/rvm\" && cd #{path} && :job'"
 set :output, {:error => 'log/error.log', :standard => 'log/cron.log'}
 
 # sitemap refresh
 every 1.day, :at => '5:00 am' do
-  rake "sitemap:refresh"
+  rake 'sitemap:refresh'
 end
 
-# git pull
-# every '0,15,30,45 * * * *' do  
+# git pull and bundle install
 every 5.minutes do
-  command "cd #{path} && git pull https://github.com/meetupedia/OpenMeetup.git master"
-end
-
-# bundle install
-# every '5,20,35,50 * * * *' do
-every 5.minutes do
-  command "source \"$HOME/.rvm/scripts/rvm\" && cd #{path} && bundle install"
+  command 'git pull https://github.com/meetupedia/OpenMeetup.git master && bundle install'
 end
 
 # whenever update cron
-# every '10,25,40,55 * * * *' do
 every 5.minutes do
-  command "source \"$HOME/.rvm/scripts/rvm\" && cd #{path} && whenever --update-crontab"
+  command 'whenever --update-crontab'
 end
 
 # i am still alive?
-# every 10.minutes do
-#  runner 'UserMailer.set_admin(User.find_by_email("andris@szimpatikus.hu")).deliver'
-# end
-
 every 5.minutes do
-  command "source \"$HOME/.rvm/scripts/rvm\" && cd #{path} && script/rails runner \"TestMailer.report('bence.nagy@gmail.com').deliver\""
+  runner "TestMailer.report('bence.nagy@gmail.com').deliver"
 end
