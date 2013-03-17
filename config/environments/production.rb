5bdad10e5e3b9025f03d816c11a758885286b982
frkandris <andris@szimpatikus.hu>
2013-03-17 16:34:51 +0100

    experimenting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,9 @@ Openmeetup::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
+
+  config.assets.precompile += %w( *.css *.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true
