40da533958522b4523cd02228c154f3f5c0662c9
Nagy Bence <nagybence@tipogral.hu>
2013-04-15 16:59:19 +0200

    trying to fix mailer styles bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require fancybox
 //= require jquery-fileupload/basic
 //= require rails-timeago-all
 //= require_tree .
-//= require twitter/bootstrap
