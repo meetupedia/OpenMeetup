a86a51e5cecda0aa34941a659de17f08eca54b78
frkandris <andris@szimpatikus.hu>
2013-04-19 10:23:13 +0200

    ericsson debug stop

--- config/schedule.rb
+++ config/schedule.rb
@@ -17,14 +17,14 @@ every 5.minutes do
 end
 
 # i am still alive?
-every 5.minutes do
-  command 'tail -n 2000 log/production.log > log/production_small.log'
-  command 'tail -n 2000 log/whenever-error.log > log/whenever-error_small.log'
-  runner "TestMailer.report('debug@meetupedia.org').deliver"
-end
+# every 5.minutes do
+#   command 'tail -n 2000 log/production.log > log/production_small.log'
+#   command 'tail -n 2000 log/whenever-error.log > log/whenever-error_small.log'
+#   runner "TestMailer.report('debug@meetupedia.org').deliver"
+# end
 
 # creating crash
-every 5.minutes do
-  command 'wget http://172.31.28.243/crash'
+# every 5.minutes do
+#   command 'wget http://172.31.28.243/crash'
 #   command 'wget http://meetupedia.com/crash'
-end
+# end
