723641b55b209ea0452445940d5b146c79927688
Nagy Bence <nagybence@tipogral.hu>
2013-05-06 14:18:04 +0200

    fix

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,8 +4,9 @@ class AdminMailer < CommonMailer
 
   def feedback(feedback_id, user_id)
     if @feedback = Feedback.find_by_id(feedback_id) and @recipient = User.find_by_id(user_id)
-      @email = @recipient.email
-      mail :to => @email, :subject => 'New feedback'
+      if @email = @recipient.email
+        mail :to => @email, :subject => 'New feedback'
+      end
     end
   end
 
