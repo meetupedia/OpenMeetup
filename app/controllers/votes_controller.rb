03ba5f0d69ae6e2946830b6ff76aa8e2d3f07707
frkandris <andris@szimpatikus.hu>
2013-05-10 16:49:51 +0200

    activity feed entry for votes on posts

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -14,6 +14,7 @@ class VotesController < CommonController
     if @vote.voteable
       if @vote.new_record?
         @vote.save
+        create_activity @vote
       else
         @vote.destroy
       end
