433da370c50ca186a7307f6a30a1392445a6bd23
Nagy Bence <nagybence@tipogral.hu>
2013-06-13 14:39:46 +0200

    admin fixes

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -10,6 +10,7 @@ class Admin
 
   def self.cron_daily
     Event.find_each { |event| event.set_counters :participations }
+    Authentication.find_each { |authentication| authentication.set_user_facebook_id }
 
     Activity.find_each { |activity| activity.set_counters :comments }
     Event.find_each { |event| event.set_counters :comments }
