df5268f03203318b32279c303beac29b86cf4d02
Nagy Bence <nagybence@tipogral.hu>
2013-08-06 11:18:40 +0200

    fix

--- mailman.rb
+++ mailman.rb
@@ -23,7 +23,9 @@ Mailman::Application.run do
   to '%group%@moly.hu' do
     puts message.inspect
     if user = User.find_by_email(message[:from])
+      puts user.inspect
       if group = Group.find_by_permalink(params[:group])
+        puts group.inspect
         group.posts.create post: message.body, user: user
       end
     end
