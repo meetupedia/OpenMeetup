c9ade5acf54ecbbdae5cefe666ea16b44130513d
Nagy Bence <nagybence@tipogral.hu>
2013-09-03 17:04:22 +0200

    events

--- app/controllers/discovery_controller.rb
+++ app/controllers/discovery_controller.rb
@@ -2,6 +2,7 @@
 
 class DiscoveryController < CommonController
   before_filter :authenticate
+  before_filter :set_city, only: [:index, :events]
 
   def index
     unless params[:q].blank?
@@ -12,7 +13,7 @@ class DiscoveryController < CommonController
         when 'members' then 'memberships_count DESC'
         else 'id ASC'
       end
-      @groups = Group.order(order)
+      @groups = Group.order(order).paginate page: params[:page]
       if Settings.group_discovery_min_member_count and Settings.group_discovery_mandatory_header_image
         @groups = @groups.where('memberships_count >= ?', Settings.group_discovery_min_member_count).where('image_updated_at IS NOT ?', nil)
       elsif Settings.group_discovery_min_member_count
@@ -20,26 +21,31 @@ class DiscoveryController < CommonController
       elsif Settings.group_discovery_mandatory_header_image
         @groups = @groups.where('image_updated_at IS NOT ?', nil)
       end
-      unless Settings.standalone
-        @city = params[:group].andand[:city_id] ? City.find_by_id(params[:group][:city_id]) : current_user.city
-        @groups = @groups.joins(:city).where('groups.city_id' => @city.id)
-      end
-      @groups = @groups.paginate page: params[:page]
+      @groups = @groups.joins(:city).where('groups.city_id' => @city.id) unless Settings.standalone
     end
   end
 
   def events
+    @events = Event.paginate page: params[:page]
+    @events = @events.joins(group: :city).where('groups.city_id' => @city.id) unless Settings.standalone
   end
 
   def friends
-  end
-
-  def groups
   end
 
   def interests
   end
 
   def newsfeed
+  end
+
+private
+
+  def set_city
+    unless Settings.standalone
+      session[:city_id] ||= current_user.city_id
+      session[:city_id] = params[:city][:id] if params[:city].andand[:id]
+      @city = City.where(id: session[:city_id]).first
+    end
   end
 end
